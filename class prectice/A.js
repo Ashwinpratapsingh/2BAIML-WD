7e7ef1e0ee22e0b5845f18cc684482cd087ff549
Ashwinpratapsingh <apsingh312007@gmail.com>
2026-09-01 11:47:30 +0530

    js prectice

--- class prectice/A.js
+++ class prectice/A.js
@@ -18,5 +18,21 @@ let result = add(10,20);
 console.log("Sum = ",result)
 
 function fullname(faname="Ashwin",Lname="Pratap Singh"){
-    return faname+" "+lname;
+    return faname+" "+Lname;
+}
+fullname()
+fullname();
+function total(s1,s2,s3){
+    return s1+s2+s3;
+}
+function add(marks){
+    return (marks/300)*100;
+}
+let totalMarks = total(80, 90, 85);  
+console.log(totalMarks);
+let percentage = add(totalMarks);
+console.log(percentage);
+
+let student = {
+    [1,2,3,4,5]
 }
