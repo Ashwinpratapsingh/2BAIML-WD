5275bf31045b9dbd22c94362ee6bcdc54ca0333f
Ashwinpratapsingh <apsingh312007@gmail.com>
2026-09-03 11:40:10 +0530

    string tamplate

---
+++ class prectice/string.js
@@ -0,0 +1,16 @@
+let Fname="Ashwin";
+let lname="Singh";
+
+console.log("My first Name is "+ Fname);
+console.log("My Last Name is "+ lname);
+console.log(`My first Name is ${Fname}`);
+console.log(`My Name is : ${Fname}${lname}`);
+
+function fulname(Fname,lname){
+    return `$(Fname) $(lname)`
+}
+
+function physics(marks){
+    return `${marks}`;
+}
+physics = (marks) =>`${marks}`;
